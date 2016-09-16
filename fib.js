6ca4e1713cc663aa08b8a93f965bf819bdb44f7c
Tony Scavone <ascavone@mediamath.com>
2016-09-16 14:07:11 -0400

    arrow funcs

---
+++ fib.js
@@ -0,0 +1,20 @@
+
+var result = [];
+function fib(n) {
+  if(n === 0){
+    return 0
+  }
+  else if(n === 1){
+    return 1;
+  }
+  else{
+    return fib(n-1) + fib(n-2)
+  }
+}
+
+(function (argument) {
+  console.log(fib(0));
+  console.log(fib(1));
+  console.log(fib(6));
+})()
+
