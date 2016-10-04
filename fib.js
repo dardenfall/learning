9132d85ef4819509ca901eedf26c85c3e0984431
Tony Scavone <ascavone@mediamath.com>
2016-10-04 15:05:28 -0400

    fib learning

--- fib.js
+++ fib.js
@@ -1,5 +1,6 @@
 
 var result = [];
+
 function fib(n) {
   if(n === 0){
     return 0
@@ -13,8 +14,28 @@ function fib(n) {
 }
 
 (function (argument) {
-  console.log(fib(0));
-  console.log(fib(1));
-  console.log(fib(6));
+  //console.log(fib(0));
+  //console.log(fib(1));
+  //console.log(fib(6));
 })()
 
+function isFib(num) {
+
+  var fibNumbers = [];
+
+  for(var i = 0; i<num*2; i++){
+    fibNumbers.push(fib(i));
+  }
+
+  console.log(fibNumbers)
+  return fibNumbers.includes(num);
+}
+
+(function () {
+
+  console.log(isFib(1))
+  console.log(isFib(2))
+  console.log(isFib(4))
+  console.log(isFib(8))
+
+})()
