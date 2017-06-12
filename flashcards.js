6f151794911bff54f2bb9a38d42d7a8f22fed327
Tony Scavone <ascavone@mediamath.com>
2017-06-12 09:21:57 -0400

    foo

---
+++ flashcards.js
@@ -0,0 +1,18 @@
+1) how do you tell array equvalence?
+  - gotta write a function
+2) how to init a javascript 2d array?
+  - normal way var a=[[a],[b]];
+3) forEach signature
+  -
+4) filter signature
+  - array.filter(function(element, index, array), thisContext);
+5) what is the signature for array.every and why use array.every or array.some?
+  - array.every(function(currentValue, indes, array), thisContext);
+6) what's the best way to iterate over all the letters of a string?
+  - normal loop
+7) does array have a contains?
+  - includes
+8) how do you declare use strict in node
+  - "use strict";
+9) how to lowercase a string?
+  - str.toLowerCase() 
