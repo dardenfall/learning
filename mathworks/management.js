a7de347b9bef9d61c4d73979c5c882950e37e8db
Tony Scavone <ascavone@mediamath.com>
2017-07-18 15:15:26 -0400

    Mgmt notes and status update

---
+++ mathworks/management.js
@@ -0,0 +1,39 @@
+
+
+Hired and mentored jess
+Built a team from one to five
+Managed in-stream refactoring while providing consistent high-quality feature delivery
+Drove the implementation of git-hooked automated test coverage
+Prioritization exercises
+
+will 
+  bored - had him write prole/mocking using websockets
+brian
+  not qualified - internal projects: mocking framework, internal tooling, qa coverage analization and reporting
+                  got him into solutions
+prianka -
+  too slow - daily checkins
+bhargavi - 
+  univested / pregnancies - found projects where she could work at her own pace, kept on top of requirment
+jeremy - 
+  nervous, unsure of himself - found him some projects from the pipeline that were 
+
+Projects:
+--------
+Segment sizes in UI
+UMT - rewrite existing peicemeal creation of pixels into a business vertical specific, creation of a set of attributes and
+  pixels
+Behavior Selector V2 - rewrite the existing hierarcy-based selector into a multitab(multi type), filter and searchable 
+  grid
+Date custom attributes - UI to allow the creation of populations based on relative and absolute dates
+  ? what are the parameters that people can use to create the ("Between, before? ")
+Standard segments - rework our existing ui to allow for a subset of functionality that correspond to a different background
+  implementation.
+Helix implementation - write the UI that allowed for the creation of lookalike / 
+  a/b modeling
+Segment Export improvements - convert existing functionality into a full-fledged list & 
+Segment Export
+Bulk Create
+Standard attributes
+
+
